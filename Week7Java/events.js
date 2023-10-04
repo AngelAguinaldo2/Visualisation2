e4ee82edb8347185ea8c84d97c7d28183fc26f5b
angelaguinaldo <126725144+angelaguinaldo@users.noreply.github.com>
2023-10-04 14:09:38 +1100

    added increment counter function to button

---
+++ Week7Java/events.js
@@ -0,0 +1,18 @@
+//function declarations
+
+function incrementCounter(){
+    //gets span element counter declared in events.html
+    var spanElement = document.getElementById('counter');
+    //creates a variable count using the span element attribute
+    var count = spanElement.getAttribute('numberOfClicks');
+    //updates count
+    count++;
+    //updates the text of the span element with current count
+    spanElement.innerText = count;
+    //updates attribute with current count
+    spanElement.setAttribute('numberOfClicks', count);
+}
+
+//script calls
+//adding event to button element 'incrementButton' -> increment counter called on click.
+document.getElementById('incrementButton').addEventListener('click',incrementCounter)
