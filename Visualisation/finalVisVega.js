81d67e07c25bdaa15c0899c4edc6ac218e055805
angelaguinaldo <126725144+angelaguinaldo@users.noreply.github.com>
2023-10-04 18:29:20 +1100

    created self guided walks

--- Visualisation/finalVisVega.js
+++ Visualisation/finalVisVega.js
@@ -3,3 +3,9 @@ vegaEmbed("#symbol_chart", vg_1).then(function(result) {
 // Access the Vega view instance 
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "self_guided_walks.vg.json";
+vegaEmbed("#walks", vg_2).then(function(result) {
+// Access the Vega view instance 
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
