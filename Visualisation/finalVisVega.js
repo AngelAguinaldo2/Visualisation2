80afe511ffcb61e1c48c5264d9df3d740c400be9
angelaguinaldo <126725144+angelaguinaldo@users.noreply.github.com>
2023-10-05 12:32:47 +1100

    edits

--- Visualisation/finalVisVega.js
+++ Visualisation/finalVisVega.js
@@ -1,5 +1,5 @@
-var vg_1 = "noise_observations_map.vg.json";
-vegaEmbed("#symbol_chart", vg_1).then(function(result) {
+var vg_1 = "bus_stops.vg.json";
+vegaEmbed("#bus_stops", vg_1).then(function(result) {
 // Access the Vega view instance 
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
