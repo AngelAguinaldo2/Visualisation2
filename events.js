0942e84bcac6c910ff7f0648f3fcd2557c6353fd
angelaguinaldo <126725144+angelaguinaldo@users.noreply.github.com>
2023-10-11 21:38:18 +1100

    fixed toggle

--- events.js
+++ events.js
@@ -1,19 +1,23 @@
+var initialMap = "self_guided_walks.vg.json";
+vegaEmbed("#toggle_map", initialMap).then(function(result) {
+// Access the Vega view instance 
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
 function walk_toggle(){
-    var busStops = document.getElementById("bus_stops");
-    var walk = document.getElementById("walks");
-    if (walk.style.display === "none") {
-        busStops.style.display = "none";
-        walk.style.display = "block";
-    } else{
-        walk.style.display = "none";
-    }
+    var vg_2 = "self_guided_walks.vg.json";
+    vegaEmbed("#toggle_map", vg_2).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
 }
 
 function bus_toggle(){
-    var busStops = document.getElementById("bus_stops");
-    var walk = document.getElementById("walks");
-    if (busStops.style.display === "none") {
-        walk.style.display = "none";
-        busStops.style.display = "block";
-    }
+    var vg_1 = "bus_stops.vg.json";
+    vegaEmbed("#toggle_map", vg_1).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
 }
